8f000a050315e253f288e35a0f01062a0e040520
lee <leerwilson@gmail.com>
2018-03-21 15:22:04 -0400

    works

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
-    output: {
-				path:__dirname + '/dist',
-				publicPath: '/',
-				filename: 'bundle.js'
-		},
-		devServer: {
-			contentBase: './dist'
-		}
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
 };
